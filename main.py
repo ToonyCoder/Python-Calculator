3aa14fd7bdbd24d316614b5fdd6258fe7bad3a5b
Toony <112717036+ToonyCoder@users.noreply.github.com>
2024-06-23 21:09:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *;
 from tkinter import messagebox;
 
 def actionauthor():
-    messagebox.showinfo("Author", "Arnav Sharma\nGrade: 5th\nSchool: CMS\nBudd Lake")
+    messagebox.showinfo("Author", "Arnav Sharma\nGrade: 6th\nSchool: MOMS\nBudd Lake")
 
 #Check weather the input string is a number or not
 def is_number(s):
